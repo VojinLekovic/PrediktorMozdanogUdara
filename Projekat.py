c52e2b53ee37fbdcd695d2318281135a4ae233e6
VojinLekovic <144007465+VojinLekovic@users.noreply.github.com>
2025-07-29 15:42:12 +0200

    Update Projekat.py

--- Projekat.py
+++ Projekat.py
@@ -40,11 +40,11 @@ y = podaci['stroke'] #
 
 
 smote = SMOTE(random_state=42)
-X_resampled, y_resampled = smote.fit_resample(X, y)
+X_prosireno, y_prosireno = smote.fit_resample(X, y)
 
 
 # Split je lijep grad
-X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_prosireno, y_prosireno, test_size=0.2, random_state=42)
 
 # Treniranje modela
 scaler = StandardScaler()
